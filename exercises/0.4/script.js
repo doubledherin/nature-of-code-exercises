aadfe94089b418fb75c9b1b3f4b10352a9735727
Wendy Dherin <doubledherin@users.noreply.github.com>
2025-02-27 17:25:51 -0800

    Consider a simulation of paint splatter drawn as a collection of colored dots. Most of the paint clusters around a central position, but some dots splatter out toward the edges. Can you use a normal distribution of random numbers to generate the positions of the dots? Can you also use a normal distribution of random numbers to generate a color palette? Try creating a slider to adjust the standard deviation.

---
+++ exercises/0.4/script.js
@@ -0,0 +1,51 @@
+let blue = "#56B7B0"
+let red = "#CC3261"
+let yellow = "#F1BC47"
+let standardDeviationSlider
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  textSize(16)
+  text(
+    "Exercise 0.4: Consider a simulation of paint splatter drawn as a collection of colored dots.",
+    10,
+    20
+  )
+  text(
+    "Most of the paint clusters around a central position, but some dots splatter out toward the edges.",
+    10,
+    40
+  )
+  text(
+    "Can you use a normal distribution of random numbers to generate the positions of the dots?",
+    10,
+    60
+  )
+  text(
+    "Can you also use a normal distribution of random numbers to generate a color palette?",
+    10,
+    80
+  )
+  text("Try creating a slider to adjust the standard deviation.", 10, 100)
+  standardDeviationSlider = createSlider(2, 60, 20, 2)
+  standardDeviationSlider.position(width - 200, 20)
+  standardDeviationSlider.size(100)
+}
+
+function draw() {
+  const sd = standardDeviationSlider.value()
+  push()
+  noStroke()
+  // fill(0)
+  text(`Standard deviation: ${sd}`, width - 200, 60)
+  pop()
+  let x = randomGaussian(width / 2, 100)
+  let y = randomGaussian(height / 2, 100)
+
+  noStroke()
+  const r = randomGaussian(255 / 2, sd)
+  const g = randomGaussian(255 / 2, sd)
+  const b = randomGaussian(255 / 2, sd)
+  fill(r, g, b)
+  circle(x, y, 16)
+}
